c57608d0a9505532069abfd5c12cfebd40bca079
TinaLM <tina.ljungestig.melander@outlook.com>
2025-02-19 12:23:26 +0100

    add app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home/")
+def home():
+    return "home"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
