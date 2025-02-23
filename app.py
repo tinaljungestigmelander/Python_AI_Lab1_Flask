7bddd993ee836a5c68a136baf630362baf1407d5
TinaLM <tina.ljungestig.melander@outlook.com>
2025-02-23 22:34:39 +0100

    add game, split into blueprints

--- app.py
+++ app.py
@@ -2,18 +2,20 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, render_template, redirect, url_for, session, flash
 from datetime import timedelta
-from blueprints.login import login_bp
+from blueprints.login import login_bp # Imports blueprints
 from blueprints.user_page import user_page_bp
 from blueprints.db import db_bp
+from blueprints.black_jack import black_jack_bp
 
 app = Flask(__name__)
 load_dotenv()
 app.secret_key=os.getenv("SECRET_KEY")
-app.permanent_session_lifetime = timedelta(minutes=5)
+app.permanent_session_lifetime = timedelta(minutes=5) # Sets how long session data is saved
 
-app.register_blueprint(login_bp, url_prefix="/login")
+app.register_blueprint(login_bp, url_prefix="/login") # Activate my blueprints
 app.register_blueprint(user_page_bp, url_prefix="/user_page")
 app.register_blueprint(db_bp, url_prefix="/db")
+app.register_blueprint(black_jack_bp, url_prefix="/black_jack")
 
 
 @app.route("/")
@@ -21,18 +23,22 @@ app.register_blueprint(db_bp, url_prefix="/db")
 def home():
     return render_template("index.html")
     
-@app.route('/blackjack')
+@app.route('/blackjack', methods=['GET','POST'])
 def blackjack():
-    return render_template("blackjack.html")        
+    if "user" in session: # Check if user is logged in before redirecting to game
+        return redirect(url_for("black_jack.set_up"))
+    else:
+        return redirect(url_for("login.login"))        
 
+# Logout method
 @app.route("/logout")
 def logout():
-    if not "user" in session:
+    if not "user" in session: # Check if user is logged in
         flash("Already logged out")
         return redirect(url_for("login.login"))
     else:
         flash("You have been logged out!", "info")
-        session.pop("user", None)   
+        session.pop("user", None) # Remove user from session when logged out   
         return redirect(url_for("login.login"))
 
 
--- blueprints/db.py
+++ blueprints/db.py
@@ -6,17 +6,18 @@ from flask import flash, Blueprint
 db_bp = Blueprint("db", __name__)
 
 load_dotenv()
-connection_string = os.getenv("CONNECTION_STRING")
+connection_string = os.getenv("CONNECTION_STRING") # Get connection string from .env file
 
-client=pymongo.MongoClient(connection_string)
-db=client["Python_AI_Lab1"]
-users=db["users"]
+client=pymongo.MongoClient(connection_string) # Sets up database
+db=client["Python_AI_Lab1"] # Name of database
+users=db["users"] # Name of collection
 
+# Method for finding user in db, placed in the db.py file because it's used by more than one blueprint
 def get(user_name):
-    user = users.find_one({"user name": user_name})  # Hämta en användare
+    user = users.find_one({"user name": user_name})  # Get user
     if user:
-        username = user["user name"]  # Hämta användarnamnet
-        password = user["password"]  # Hämta lösenordet
+        username = user["user name"]  # Get user name
+        password = user["password"]  # Get password
         return [username,password]
     else:
         flash("User not found")
